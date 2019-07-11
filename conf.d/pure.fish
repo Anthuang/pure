3eab315319554e70a266ac58a084e272f0b48a4f
Anthony Huang <anthony.huang@affirm.com>
2019-07-10 19:42:15 -0700

    Change colors

--- conf.d/pure.fish
+++ conf.d/pure.fish
@@ -1,42 +1,43 @@
 set --universal pure_version 2.1.7 # used for bug report
 
 # Base colors
-_pure_set_default pure_color_primary (set_color blue)
-_pure_set_default pure_color_info (set_color cyan)
-_pure_set_default pure_color_mute (set_color brblack)
-_pure_set_default pure_color_success (set_color magenta)
+_pure_set_default pure_color_blue (set_color blue)
+_pure_set_default pure_color_cyan (set_color cyan)
+_pure_set_default pure_color_brblack (set_color brblack)
+_pure_set_default pure_color_green (set_color green)
+_pure_set_default pure_color_magenta (set_color magenta)
 _pure_set_default pure_color_normal (set_color normal)
-_pure_set_default pure_color_danger (set_color red)
-_pure_set_default pure_color_light (set_color white)
-_pure_set_default pure_color_warning (set_color yellow)
-_pure_set_default pure_color_dark (set_color black)
+_pure_set_default pure_color_red (set_color red)
+_pure_set_default pure_color_white (set_color white)
+_pure_set_default pure_color_yellow (set_color yellow)
+_pure_set_default pure_color_black (set_color black)
 
 # Prompt
 _pure_set_default pure_symbol_prompt "❯"
 _pure_set_default pure_symbol_reverse_prompt "❮"  # used for VI mode
-_pure_set_default pure_color_prompt_on_error $pure_color_danger
-_pure_set_default pure_color_prompt_on_success $pure_color_success
+_pure_set_default pure_color_prompt_on_error $pure_color_red
+_pure_set_default pure_color_prompt_on_success $pure_color_magenta
 
 # Current Working Directory
-_pure_set_default pure_color_current_directory $pure_color_primary
+_pure_set_default pure_color_current_directory $pure_color_blue
 
 # Git
 _pure_set_default pure_symbol_git_unpulled_commits "⇣"
 _pure_set_default pure_symbol_git_unpushed_commits "⇡"
 _pure_set_default pure_symbol_git_dirty "*"
-_pure_set_default pure_color_git_unpulled_commits $pure_color_info
-_pure_set_default pure_color_git_unpushed_commits $pure_color_info
-_pure_set_default pure_color_git_branch $pure_color_mute
-_pure_set_default pure_color_git_dirty $pure_color_mute
+_pure_set_default pure_color_git_unpulled_commits $pure_color_cyan
+_pure_set_default pure_color_git_unpushed_commits $pure_color_cyan
+_pure_set_default pure_color_git_branch $pure_color_green
+_pure_set_default pure_color_git_dirty $pure_color_red
 
 # SSH info
-_pure_set_default pure_color_ssh_hostname $pure_color_mute
-_pure_set_default pure_color_ssh_separator $pure_color_mute
-_pure_set_default pure_color_ssh_user_normal $pure_color_mute
-_pure_set_default pure_color_ssh_user_root $pure_color_light
+_pure_set_default pure_color_ssh_hostname $pure_color_green
+_pure_set_default pure_color_ssh_separator $pure_color_green
+_pure_set_default pure_color_ssh_user_normal $pure_color_green
+_pure_set_default pure_color_ssh_user_root $pure_color_white
 
 # Virtualenv for Pyhon
-_pure_set_default pure_color_virtualenv $pure_color_mute
+_pure_set_default pure_color_virtualenv $pure_color_green
 
 # Print current working directory at the beginning of prompt
 # true (default):   current directory, git, user@hostname (ssh-only), command duration
@@ -50,7 +51,7 @@ _pure_set_default pure_separate_prompt_on_error false
 
 # Max execution time of a process before its run time is shown when it exits
 _pure_set_default pure_threshold_command_duration 5
-_pure_set_default pure_color_command_duration $pure_color_warning
+_pure_set_default pure_color_command_duration $pure_color_yellow
 
 # Right Prompt variables
 _pure_set_default pure_right_prompt ""
